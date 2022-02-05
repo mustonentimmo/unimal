b2a2ac6f19f8fd5a9e8ccdb60bc1b70356ca3c01
mustonentimmo <62522763+mustonentimmo@users.noreply.github.com>
2022-02-05 15:10:10 +0200

    Add custom document for applying body classes

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="mx-auto max-w-[1400px] px-3 md:px-6">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
