00c9fc4099a7494acad0eb14ee538c26aacb58f9
Timmo Mustonen <mustonentimmo@gmail.com>
2022-02-14 23:50:36 +0200

    Refactor shelter card component

--- components/ShelterCard/ShelterCard.tsx
+++ components/ShelterCard/ShelterCard.tsx
@@ -1,3 +1,6 @@
+import placeholder from '../../public/placeholder.svg'
+import Image from 'next/image'
+
 interface shelterCardProps {
   name: string
   description: string
@@ -6,11 +9,9 @@ interface shelterCardProps {
 
 const shelterCard = ({ name, description, image }: shelterCardProps) => {
   return (
-    <div className="flex-[1_1_300px] rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 md:max-w-md">
-      <a href="#">
-        <img className="rounded-t-lg" src={image} alt="" />
-      </a>
-      <div className="p-5">
+    <div className="h-fit flex-[1_1_300px] rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 md:max-w-md">
+      <div className="h-[25rem] bg-[url('/placeholder.svg')] bg-contain bg-center"></div>
+      <div className="flex h-[12rem] flex-col p-5">
         <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white">
             {name}
@@ -21,7 +22,7 @@ const shelterCard = ({ name, description, image }: shelterCardProps) => {
         </p>
         <button
           type="button"
-          className="mr-2 mb-2 rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800"
+          className="mt-auto mr-2 mb-2 self-baseline rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800"
         >
           Külasta
         </button>
