c0ada2d975098d1f5bbd56e231c6df4727c22044
Timmo Mustonen <mustonentimmo@gmail.com>
2022-02-19 13:15:31 +0200

    Add env variable for api request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 }
 
 MyApp.getInitialProps = async () => {
-  const res = await fetch(`http://65.108.153.196:1337/api/shelters/`)
+  const res = await fetch(`${process.env.API_HOST}/api/shelters/`)
   const shelters = await res.json()
 
   return { pageProps: { shelters } }
