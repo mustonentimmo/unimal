9ed8c616de12e9b98951c2a574037b719c91d82a
Timmo Mustonen <mustonentimmo@gmail.com>
2022-03-29 20:41:14 +0300

    Add shelters slice

---
+++ features/sheltersSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import axios from 'axios';
+
+export const initialState = {
+  loading: false,
+  hasError: false,
+  shelters: [],
+};
+
+const sheltersSlice = createSlice({
+  name: 'shelters',
+  initialState,
+  reducers: {
+    getShelters: (state) => {
+      state.loading = true;
+    },
+    getSheltersSuccess: (state, { payload }) => {
+      (state.loading = false), (state.hasError = false), (state.shelters = payload);
+    },
+    getSheltersFailure: (state, { payload }) => {
+      (state.loading = false), (state.hasError = true), (state.shelters = []);
+    },
+  },
+});
+
+export const { getShelters, getSheltersSuccess, getSheltersFailure } = sheltersSlice.actions;
+export const sheltersSelector = (state: RootState) => state.shelters;
+
+export default sheltersSlice.reducer;
+
+export const fetchShelters = () => {
+  return function (dispatch: Dispatch) {
+    dispatch(getShelters());
+    axios
+      .get('')
+      .then((response) => {
+        dispatch(getSheltersSuccess(response));
+      })
+      .catch((error) => {
+        dispatch(getSheltersFailure(error));
+      });
+  };
+};
