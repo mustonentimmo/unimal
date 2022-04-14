ee6bfdba85686685ac85567bc41cb6bb3ebcb312
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-14 13:59:24 +0300

    Add helpers

--- shared/helpers.ts
+++ shared/helpers.ts
@@ -16,3 +16,57 @@ export const counties = [
   { value: 'voru', label: 'Võru' },
   { value: 'valga', label: 'Valga' },
 ];
+
+export const animalSortOptions = [
+  { name: 'äsja saabunud', href: '#', current: false },
+  { name: 'pikaajalised', href: '#', current: false },
+];
+
+export const animalFilters = [
+  {
+    id: 'species',
+    name: 'Liik',
+    options: [
+      { value: 'koer', label: 'Koer', checked: false },
+      { value: 'kass', label: 'Kass', checked: false },
+      { value: 'muu', label: 'Muu', checked: false },
+    ],
+  },
+  {
+    id: 'sex',
+    name: 'Sugu',
+    options: [
+      { value: 'isane', label: 'Isane', checked: false },
+      { value: 'emane', label: 'Emane', checked: false },
+    ],
+  },
+  {
+    id: 'color',
+    name: 'Värvus',
+    options: [
+      { value: 'must', label: 'Must', checked: false },
+      { value: 'valge', label: 'Valge', checked: false },
+      { value: 'punane', label: 'Punane', checked: false },
+      { value: 'hall', label: 'Hall', checked: false },
+      { value: 'kirju', label: 'Kirju', checked: false },
+    ],
+  },
+  {
+    id: 'size',
+    name: 'Suurus',
+    options: [
+      { value: 'pisike', label: 'Pisike', checked: false },
+      { value: 'keskmine', label: 'Keskmine', checked: false },
+      { value: 'suur', label: 'Suur', checked: false },
+    ],
+  },
+  {
+    id: 'age',
+    name: 'Vanus',
+    options: [
+      { value: 'juunior', label: 'Juunior', checked: false },
+      { value: 'täiskasvanu', label: 'Täiskasvanu', checked: false },
+      { value: 'seenior', label: 'Seenior', checked: false },
+    ],
+  },
+];
