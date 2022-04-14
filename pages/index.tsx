5a173f81a78a09a0102d086ba5964cd540f802e0
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-14 13:35:32 +0300

    Fetch data from state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,44 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Filter from '@/components/Filter/Filter';
 import Hero from '@/components/Hero/Hero';
 import ShelterCard from '@/components/ShelterCard/ShelterCard';
+import { Spinner } from '@/components/Spinner/Spinner';
 
-const Home = ({ shelters }: any) => {
-  const totalShelter = shelters.data.length;
+import { fetchShelters, loadingSelector, sheltersSelector } from '../features/sheltersSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const shelters = useSelector(sheltersSelector);
+  const isLoading = useSelector(loadingSelector);
+
+  useEffect(() => {
+    dispatch(fetchShelters());
+  }, [dispatch]);
+
+  const totalShelters = shelters.length;
 
   return (
     <>
       <Hero />
       <section id="shelter" className="mt-[3rem]">
-        <div className="text-center text-2xl font-bold">Varjupaigad ({totalShelter})</div>
+        <div className="text-center text-2xl font-bold">Varjupaigad ({totalShelters})</div>
         <Filter />
         <div className="flex flex-wrap gap-5 py-7">
-          {shelters.data.map((shelter: any) => (
-            <ShelterCard
-              key={shelter.id}
-              id={shelter.id}
-              name={shelter.attributes.shelter_name}
-              description={shelter.attributes.shelter_description}
-              image={shelter.attributes.shelter_image}
-            />
-          ))}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            shelters.map((shelter: any) => (
+              <ShelterCard
+                key={shelter.id}
+                id={shelter.id}
+                name={shelter.attributes.shelter_name}
+                description={shelter.attributes.shelter_description}
+                image={shelter.attributes.shelter_image}
+              />
+            ))
+          )}
         </div>
       </section>
     </>
