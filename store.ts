d5a47fc19d76fef855672c95675bba07ea018689
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-17 15:24:32 +0300

    Add filters reducer to the redux store

--- store.ts
+++ store.ts
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import filtersReducer from './features/filtersSlice';
 import sheltersReducer from './features/sheltersSlice';
 
 export const store = configureStore({
   reducer: {
     shelters: sheltersReducer,
+    filters: filtersReducer,
   },
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
