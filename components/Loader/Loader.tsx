8f25bcc327eb261f43ab66ce5e47883e10026897
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-17 15:23:59 +0300

    Refactor component name

---
+++ components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+import { SpinnerRoundFilled } from 'spinners-react';
+
+import { spinnerStyle } from '@/shared/customStyles';
+
+export const Loader = () => <SpinnerRoundFilled color="#1a56db" style={spinnerStyle} />;
