08a76ce79e67093fa0c7de29a7774ebe0edfe91d
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-19 22:07:36 +0300

    Add todo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+// TODO: Test if next transpile module causing performance issues (example affecting page loading time)
 const withTM = require('next-transpile-modules')(['@uiball/loaders']);
 
 module.exports = withTM({
