db417e48980cebee3710b63258926f4186911c4f
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-20 00:30:35 +0300

    Add animals filter reducers

--- features/filtersSlice.ts
+++ features/filtersSlice.ts
@@ -2,7 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const initialState = {
   locationFilter: '',
-  animalsFilter: {},
+  animalsFilter: {
+    species: [],
+    sex: [],
+    size: [],
+    color: [],
+    character: [],
+  },
 };
 
 const filtersSlice = createSlice({
@@ -12,12 +18,55 @@ const filtersSlice = createSlice({
     setLocationFilter: (state, { payload }) => {
       state.locationFilter = payload.value;
     },
-    setAnimalsFilter: () => {},
+    addAnimalsFilter: (state, { payload }) => {
+      if (payload.section === 'species') {
+        state.animalsFilter.species = [...state.animalsFilter.species, payload.value];
+      }
+      if (payload.section === 'sex') {
+        state.animalsFilter.sex = [...state.animalsFilter.sex, payload.value];
+      }
+      if (payload.section === 'size') {
+        state.animalsFilter.size = [...state.animalsFilter.size, payload.value];
+      }
+      if (payload.section === 'color') {
+        state.animalsFilter.color = [...state.animalsFilter.color, payload.value];
+      }
+      if (payload.section === 'character') {
+        state.animalsFilter.character = [...state.animalsFilter.character, payload.value];
+      }
+    },
+    removeAnimalsFilter: (state, { payload }) => {
+      if (payload.section === 'species') {
+        state.animalsFilter.species = state.animalsFilter.species.filter(
+          (value) => value !== payload.value
+        );
+      }
+      if (payload.section === 'sex') {
+        state.animalsFilter.sex = state.animalsFilter.sex.filter(
+          (value) => value !== payload.value
+        );
+      }
+      if (payload.section === 'size') {
+        state.animalsFilter.size = state.animalsFilter.size.filter(
+          (value) => value !== payload.value
+        );
+      }
+      if (payload.section === 'color') {
+        state.animalsFilter.color = state.animalsFilter.color.filter(
+          (value) => value !== payload.value
+        );
+      }
+      if (payload.section === 'character') {
+        state.animalsFilter.character = state.animalsFilter.character.filter(
+          (value) => value !== payload.value
+        );
+      }
+    },
   },
 });
 
-export const { setLocationFilter, setAnimalsFilter } = filtersSlice.actions;
-export const locationFilterSelector = (state) => state.filters.locationFilter;
-export const locationAnimalsFilterSelector = (state) => state.filters.animalsFilter;
+export const { setLocationFilter, addAnimalsFilter, removeAnimalsFilter } = filtersSlice.actions;
+export const locationFilterSelector = (state: any) => state.filters.locationFilter;
+export const animalsFilterSelector = (state: any) => state.filters.animalsFilter;
 
 export default filtersSlice.reducer;
