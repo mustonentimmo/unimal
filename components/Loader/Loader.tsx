55434bd8bcc999fa8f40f58e7bc4bd0ebebe5e15
Timmo Mustonen <mustonentimmo@gmail.com>
2022-04-19 21:18:05 +0300

    Remove unnecessary component

--- components/Loader/Loader.tsx
+++
@@ -1,5 +0,0 @@
-import { SpinnerRoundFilled } from 'spinners-react';
-
-import { spinnerStyle } from '@/shared/customStyles';
-
-export const Loader = () => <SpinnerRoundFilled color="#1a56db" style={spinnerStyle} />;
