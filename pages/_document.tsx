e5c2eb8bae0f5fea4ea62fd05cffd880588b3970
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-03 21:25:19 +0300

    Add header to _document component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+import Header from '@/components/Header/Header';
 
 export default function Document() {
   return (
     <Html className="scroll-smooth text-gray-900">
       <Head />
       <body className="mx-auto max-w-[1400px] px-3 md:px-9">
+        <Header />
         <Main />
         <NextScript />
       </body>
