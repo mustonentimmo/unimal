ebea690a1b8e98ec41265b84e05a046ee5848df3
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-03 23:04:23 +0300

    Remove layout wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,12 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
-import Layout from '@/components/Layout/Layout';
-
 import { store } from '../store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </Provider>
   );
 };
