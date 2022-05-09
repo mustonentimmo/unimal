97720730fd29f4d02dce12c0c373a900d5c885cd
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-09 20:26:51 +0300

    Refactor ShelterCard component

--- components/ShelterCard/ShelterCard.tsx
+++ components/ShelterCard/ShelterCard.tsx
@@ -1,41 +1,52 @@
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { FaDog } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
 
 interface shelterCardProps {
   id: number;
   name: string;
+  location: string;
   description: string;
+  animalsCount: number;
   image: string;
 }
 
-const shelterCard = ({ name, description, image, id }: shelterCardProps) => {
+const shelterCard = ({
+  name,
+  description,
+  image,
+  id,
+  animalsCount,
+  location,
+}: shelterCardProps) => {
   return (
-    <motion.div
-      layout
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-      className="h-fit rounded-lg bg-white shadow-lg"
-    >
-      <div className="h-[25rem] rounded-lg bg-[url('/placeholder.svg')] bg-contain bg-center"></div>
-      <div className="flex h-[12rem] flex-col p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white">{name}</h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 line-clamp-2 dark:text-gray-400">
-          {description}
-        </p>
-        <Link href={`/shelter/${id}`}>
-          <button
-            type="button"
-            className="mt-auto mr-2 mb-2 self-baseline rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800"
-          >
-            Külasta
-          </button>
-        </Link>
-      </div>
-    </motion.div>
+    <Link href={`/shelter/${id}`}>
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        className="h-fit cursor-pointer rounded-lg bg-white shadow-lg"
+      >
+        <div className="h-[20rem] rounded-lg bg-[url('/placeholder.svg')] bg-contain bg-center"></div>
+        <div className="flex h-[13rem] flex-col p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight">{name}</h5>
+          <div className="flex">
+            <div className="inline-block flex justify-center self-start rounded bg-gradient-to-r from-sky-400 to-cyan-300 px-4 py-2 align-middle text-sm text-white">
+              <FaDog size="1.25em" />
+              <span className="ml-1 font-medium capitalize">{animalsCount}</span>
+            </div>
+            <div className="ml-1 inline-block flex justify-center self-start rounded bg-gradient-to-br from-rose-400 to-orange-300 px-4 py-2 align-middle text-sm text-white">
+              <MdLocationOn size="1.25em" />
+              <span className="ml-1 font-medium capitalize">{location.county}</span>
+            </div>
+          </div>
+          <p className="mt-4 font-normal text-gray-700 line-clamp-4">{description}</p>
+        </div>
+      </motion.div>
+    </Link>
   );
 };
 
