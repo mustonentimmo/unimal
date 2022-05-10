2f89a62e529b71e122103c7e99b53ae1b8fad20e
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-10 21:44:15 +0300

    Add utility functions

--- shared/utilities.ts
+++ shared/utilities.ts
@@ -1,3 +1,21 @@
 export const getFullAPIUrl = (path: string) => {
   return process.env.API_HOST + path;
 };
+
+export const getShelterDataByID = (shelters: any, id: string | string[] | undefined) => {
+  return (
+    shelters.length > 0 &&
+    shelters.filter((shelter: any) => {
+      return shelter.id == id;
+    })['0'].attributes
+  );
+};
+
+export const getAnimalDataByID = (animals: any, id: string | string[] | undefined) => {
+  return (
+    animals.length > 0 &&
+    animals.filter((animal: any) => {
+      return animal.id == id;
+    })
+  );
+};
