a3d8321b6dd2bd2542c996b74b12a10e760be269
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-10 21:40:52 +0300

    Add animal profile to AnimalCard component

--- components/AnimalCard/AnimalCard.tsx
+++ components/AnimalCard/AnimalCard.tsx
@@ -1,12 +1,10 @@
 const AnimalCard = ({ name }) => {
   return (
     <div className="relative h-[17rem] cursor-pointer overflow-hidden rounded-2xl drop-shadow-md hover:drop-shadow-lg">
-      <img
-        className="h-full w-full"
-        src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
-        alt=""
-      />
-      <h1 className="absolute bottom-0 left-4 p-2 text-2xl font-bold text-white">{name}</h1>
+      <h1 className="absolute bottom-0 left-4 z-[1] p-2 text-2xl font-bold text-white drop-shadow-md">
+        {name}
+      </h1>
+      <Image objectFit="cover" layout="fill" src={profilePicture} />
     </div>
   );
 };
