b12fd6dbe079bda28dea8fbfb83f3748762a3fba
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-10 21:41:36 +0300

    Add type to AnimalCard component props

--- components/AnimalCard/AnimalCard.tsx
+++ components/AnimalCard/AnimalCard.tsx
@@ -1,4 +1,11 @@
-const AnimalCard = ({ name }) => {
+import Image from 'next/image';
+
+interface AnimalCardProps {
+  name: string;
+  profilePicture: string;
+}
+
+const AnimalCard = ({ name, profilePicture }: AnimalCardProps) => {
   return (
     <div className="relative h-[17rem] cursor-pointer overflow-hidden rounded-2xl drop-shadow-md hover:drop-shadow-lg">
       <h1 className="absolute bottom-0 left-4 z-[1] p-2 text-2xl font-bold text-white drop-shadow-md">
