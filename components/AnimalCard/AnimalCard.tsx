6b733da06b3274715b5c4d41afd909be987b108d
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-14 20:29:41 +0300

    Add link to shelterCard

--- components/AnimalCard/AnimalCard.tsx
+++ components/AnimalCard/AnimalCard.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface AnimalCardProps {
   name: string;
   profilePicture: string;
 }
 
-const AnimalCard = ({ name, profilePicture }: AnimalCardProps) => {
+const AnimalCard = ({ name, profilePicture, shelterID, animalID }: AnimalCardProps) => {
   return (
-    <div className="relative h-[17rem] cursor-pointer overflow-hidden rounded-2xl drop-shadow-md hover:drop-shadow-lg">
-      <h1 className="absolute bottom-0 left-4 z-[1] p-2 text-2xl font-bold text-white drop-shadow-md">
-        {name}
-      </h1>
-      <Image objectFit="cover" layout="fill" src={profilePicture} />
-    </div>
+    <Link href={`/shelter/${shelterID}/animal/${animalID}`}>
+      <div className="relative h-[17rem] cursor-pointer overflow-hidden rounded-2xl drop-shadow-md hover:drop-shadow-lg">
+        <h1 className="absolute bottom-0 left-4 z-[1] p-2 text-2xl font-bold text-white drop-shadow-md">
+          {name}
+        </h1>
+        <Image objectFit="cover" layout="fill" src={profilePicture} />
+      </div>
+    </Link>
   );
 };
 
