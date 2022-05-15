a50661e4bc785422e37325b8fe95c18e2822c15b
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-15 21:29:27 +0300

    Add visual changes to ShelterCard component

--- components/ShelterCard/ShelterCard.tsx
+++ components/ShelterCard/ShelterCard.tsx
@@ -23,7 +23,7 @@ const shelterCard = ({ name, description, logo, id, animalsCount, county }: shel
         exit={{ opacity: 0 }}
         transition={{ duration: 0.3 }}
         id={`shelter-${id}`}
-        className="h-fit cursor-pointer rounded-lg bg-white shadow-lg"
+        className="h-fit cursor-pointer rounded-lg bg-white shadow-md hover:shadow-xl"
       >
         <div className="flex h-[20rem] min-w-full items-center justify-center overflow-hidden rounded-lg">
           <Image src={logo} height="200px" width="200px" />
@@ -31,13 +31,13 @@ const shelterCard = ({ name, description, logo, id, animalsCount, county }: shel
         <div className="flex h-[13rem] flex-col p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight">{name}</h5>
           <div className="flex">
-            <div className="inline-block flex justify-center self-start rounded bg-gradient-to-r from-sky-400 to-cyan-300 px-4 py-2 align-middle text-sm text-white">
-              <FaDog size="1.25em" />
-              <span className="ml-1 font-medium capitalize">{animalsCount}</span>
+            <div className="inline-block flex justify-center self-start rounded bg-gray-50 px-4 py-2 align-middle text-sm text-white">
+              <FaDog size="1.25em" className="text-indigo-600" />
+              <span className="ml-1 font-medium capitalize text-black">{animalsCount}</span>
             </div>
-            <div className="ml-1 inline-block flex justify-center self-start rounded bg-gradient-to-br from-rose-400 to-orange-300 px-4 py-2 align-middle text-sm text-white">
-              <MdLocationOn size="1.25em" />
-              <span className="ml-1 font-medium capitalize">{county}</span>
+            <div className="ml-1 inline-block flex justify-center self-start rounded bg-gray-50 px-4 py-2 align-middle text-sm text-white">
+              <MdLocationOn size="1.25em" className="text-indigo-600" />
+              <span className="ml-1 font-medium capitalize text-black">{county}</span>
             </div>
           </div>
           <p className="mt-4 font-normal text-gray-700 line-clamp-4">{description}</p>
