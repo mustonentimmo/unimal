2e72de58a196c3f61b57237bd4ec9a53e4d03f6b
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-16 23:57:04 +0300

    Remove meta tag from the head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { Provider } from 'react-redux';
 
 import { store } from '../store';
@@ -9,9 +8,6 @@ import { store } from '../store';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
       <Component {...pageProps} />
     </Provider>
   );
