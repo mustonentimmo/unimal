990d4269d4a1266e7a83e0f576231ae08135eb4a
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-16 22:43:09 +0300

    Add meta tag to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 
 import { store } from '../store';
@@ -8,6 +9,9 @@ import { store } from '../store';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
