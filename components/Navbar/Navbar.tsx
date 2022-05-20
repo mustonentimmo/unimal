ca77d9da5784ac5e93537ee4bd3ea567bfee4669
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-20 22:17:58 +0300

    Add visual fixes to Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="rounded border-gray-200 bg-white px-2 py-4 sm:px-4">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+    <nav className="rounded border-gray-200 bg-white py-4">
+      <div className="flex flex-wrap items-center justify-between">
         <Link href="/">
           <a className="flex items-center">
             <span
@@ -30,7 +30,7 @@ const Navbar = () => {
           <button
             id="navbar_cta_button"
             type="button"
-            className="mr-2 mb-2 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+            className="rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             <a href="http://65.108.153.196:1337/admin" target="_blank" rel="noreferrer">
               Infosüsteem
@@ -40,7 +40,7 @@ const Navbar = () => {
           <button
             data-collapse-toggle="mobile-menu-4"
             type="button"
-            className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
             aria-controls="mobile-menu-4"
             aria-expanded="false"
           >
