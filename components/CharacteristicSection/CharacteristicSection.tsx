f37893ab49565876421f27a45dd64a20edd71fe3
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-21 16:05:27 +0300

    Refactor CharacteristicSection component

--- components/CharacteristicSection/CharacteristicSection.tsx
+++ components/CharacteristicSection/CharacteristicSection.tsx
@@ -31,30 +31,17 @@ const CharacteristicsSection = ({ characteristics }: CharacteristicsSectionProps
   };
 
   return (
-    <div className="py-12">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2 className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-            Iseloomujooned
-          </h2>
-        </div>
-        <div className="mt-10">
-          <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
-            {getCharacteristics(characteristics).map((character) => (
-              <div key={character.value} className="relative">
-                <dt>
-                  <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                    <character.icon className="h-6 w-6" aria-hidden="true" />
-                  </div>
-                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
-                    {character.value}
-                  </p>
-                </dt>
-              </div>
-            ))}
-          </dl>
-        </div>
-      </div>
+    <div className="mt-2">
+      <dl className="grid grid-cols-1 gap-y-5 md:grid-cols-2 md:gap-x-8">
+        {getCharacteristics(characteristics).map((character) => (
+          <div key={character.value} className="flex items-center">
+            <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+              <character.icon className="h-6 w-6" aria-hidden="true" />
+            </div>
+            <p className="text-md ml-3 font-medium">{character.value}</p>
+          </div>
+        ))}
+      </dl>
     </div>
   );
 };
