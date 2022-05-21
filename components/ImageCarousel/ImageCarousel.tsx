3c7357fdb3b7d7e8c9dfaecf114ac1c4db22a740
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-21 16:17:50 +0300

    Add ImageCarousel component

---
+++ components/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,69 @@
+import cn from 'classnames';
+import Autoplay from 'embla-carousel-autoplay';
+import useEmblaCarousel from 'embla-carousel-react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+
+import styles from './ImageCarousel.module.scss';
+
+const ImageCarousel = ({ images }) => {
+  const options = { delay: 4000, stopOnInteraction: true }; // Options
+  const autoplayRoot = (emblaRoot) => emblaRoot.parentElement; // Root node
+  const autoplay = Autoplay(options, autoplayRoot);
+
+  const [viewportRef, embla] = useEmblaCarousel(
+    {
+      loop: true,
+      skipSnaps: false,
+    },
+    [autoplay]
+  );
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setPrevBtnEnabled(embla.canScrollPrev());
+    setNextBtnEnabled(embla.canScrollNext());
+  }, [embla]);
+
+  useEffect(() => {
+    if (!embla) return;
+    embla.on('select', onSelect);
+    onSelect();
+  }, [embla, onSelect]);
+
+  return (
+    <div className={styles['embla']}>
+      <div className={styles['embla__viewport']} ref={viewportRef}>
+        <div className={styles['embla__container']}>
+          {images.map((src, index) => (
+            <div className={styles['embla__slide']} key={index}>
+              <div className={styles['embla__slide__inner']}>
+                <img className={styles['embla__slide__img']} src={src} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button
+        className={cn(styles['embla__button'], styles['embla__button--prev'])}
+        onClick={scrollPrev}
+        disabled={!prevBtnEnabled}
+      >
+        <IoIosArrowBack />
+      </button>
+      <button
+        className={cn(styles['embla__button'], styles['embla__button--next'])}
+        onClick={scrollNext}
+        disabled={!nextBtnEnabled}
+      >
+        <IoIosArrowBack />
+      </button>
+    </div>
+  );
+};
+
+export default ImageCarousel;
