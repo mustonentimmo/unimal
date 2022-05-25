a788d1daec03ce264fe92012642ef93f9f525e9b
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-25 22:59:24 +0300

    Add pageHeader component

--- components/PageHeader/PageHeader.tsx
+++ components/PageHeader/PageHeader.tsx
@@ -13,10 +13,9 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }: PageHeaderProps) => {
     <div className="flex h-[6rem] items-center justify-center pt-10 text-indigo-600">
       <a
         onClick={() => router.back()}
-        className="mr-auto flex cursor-pointer items-center justify-center"
+        className="mr-auto flex cursor-pointer items-center justify-center rounded-md p-2 shadow-md duration-100 ease-in hover:scale-90 hover:opacity-90"
       >
-        <IoIosArrowBack />
-        <span className="text-lg font-medium">Tagasi</span>
+        <IoIosArrowBack size="2rem" />
       </a>
       {title && <h1 className="mr-auto text-[3rem] font-extrabold capitalize">{title}</h1>}
     </div>
