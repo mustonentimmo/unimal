fd82b42b9335714f29e265e8313ee06c53cc58ac
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-26 17:20:23 +0300

    Add Footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="rounded-lg bg-white p-4 md:px-6 md:py-8">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com" className="mb-4 flex items-center sm:mb-0"></a>
+      </div>
+      <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+      <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        © 2022 Unimal. All Rights Reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
+import Footer from '@/components/Footer/Footer';
 import Navbar from '@/components/Navbar/Navbar';
 
 export default function Document() {
@@ -10,6 +11,7 @@ export default function Document() {
       <body className="mx-auto max-w-[1400px] px-3 md:px-9">
         <Navbar />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>
