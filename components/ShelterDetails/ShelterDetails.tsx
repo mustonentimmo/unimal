3a1d4a43bc73ab5c5ee2810cdb1e9b1e0d319a7d
Timmo Mustonen <mustonentimmo@gmail.com>
2022-05-26 17:25:26 +0300

    Fix ShelterDetails component spacing

--- components/ShelterDetails/ShelterDetails.tsx
+++ components/ShelterDetails/ShelterDetails.tsx
@@ -49,27 +49,25 @@ const ShelterDetails = ({
   ];
 
   return (
-    <div className="bg-white">
-      <div className="grid grid-cols-1 items-center gap-y-[3rem] gap-x-[3rem] lg:grid-cols-2">
-        <div>
-          <h2 className="text-3xl font-extrabold tracking-tight">Üldinfo</h2>
-          <p className="mt-4 text-gray-500">{description}</p>
-          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-            {shelterInfo.map((item, index) => (
-              <div key={index} className="border-t border-gray-200 pt-4">
-                <dt className="font-medium text-indigo-600">{item.title}</dt>
-                {item.context.length > 0 &&
-                  item['context'].map((nested_item: any, index) => (
-                    <dd key={index} className="mt-2 text-sm text-gray-500">
-                      {nested_item.title && <b>{nested_item.title}:</b>} {nested_item.value}
-                    </dd>
-                  ))}
-              </div>
-            ))}
-          </dl>
-        </div>
-        <ImageCarousel images={getGalleryData(pictures)} />
+    <div className="mb-10 grid grid-cols-1 items-center gap-y-[3rem] gap-x-[3rem] lg:grid-cols-2">
+      <div>
+        <h2 className="text-3xl font-extrabold tracking-tight">Üldinfo</h2>
+        <p className="mt-4 text-gray-500">{description}</p>
+        <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          {shelterInfo.map((item, index) => (
+            <div key={index} className="border-t border-gray-200 pt-4">
+              <dt className="font-medium text-indigo-600">{item.title}</dt>
+              {item.context.length > 0 &&
+                item['context'].map((nested_item: any, index) => (
+                  <dd key={index} className="mt-2 text-sm text-gray-500">
+                    {nested_item.title && <b>{nested_item.title}:</b>} {nested_item.value}
+                  </dd>
+                ))}
+            </div>
+          ))}
+        </dl>
       </div>
+      <ImageCarousel images={getGalleryData(pictures)} />
     </div>
   );
 };
