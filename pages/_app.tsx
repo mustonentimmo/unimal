92983868640c54148b030b56ea2f521effe7af5c
Timmo Mustonen <mustonentimmo@gmail.com>
2022-06-04 16:07:24 +0300

    Add scroll to top functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,14 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
-
-import ScrollToTop from '@/components/ScrollToTop/scrollToTop';
 
 import { store } from '../store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [isVisible, setIsVisible] = useState(false);
-  console.log(isVisible);
-
-  useEffect(() => {
-    const toggleVisibility = () =>
-      window.scrollY > 1000 ? setIsVisible(true) : setIsVisible(false);
-
-    window.addEventListener('scroll', toggleVisibility);
-
-    return () => window.removeEventListener('scroll', toggleVisibility);
-  }, []);
-
   return (
     <Provider store={store}>
       <Component {...pageProps} />
-      {isVisible && <ScrollToTop />}
     </Provider>
   );
 };
